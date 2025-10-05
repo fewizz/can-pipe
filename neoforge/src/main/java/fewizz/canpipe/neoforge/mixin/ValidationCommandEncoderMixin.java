c816c70e1650617c54a9fe47de139d377bc4170a
fewizz <hazeevaidar@gmail.com>
2025-10-05 14:35:17 +0300

    fix neoforge validation layer compat

--- neoforge/src/main/java/fewizz/canpipe/neoforge/mixin/ValidationCommandEncoderMixin.java
+++ neoforge/src/main/java/fewizz/canpipe/neoforge/mixin/ValidationCommandEncoderMixin.java
@@ -17,18 +17,33 @@ import fewizz.canpipe.b3d.CommandEncoderExtended;
 import net.neoforged.neoforge.client.blaze3d.validation.GpuDeviceUsageValidator;
 import net.neoforged.neoforge.client.blaze3d.validation.ValidationCommandEncoder;
 import net.neoforged.neoforge.client.blaze3d.validation.ValidationGpuTexture;
+import net.neoforged.neoforge.client.blaze3d.validation.ValidationGpuTextureView;
+import net.neoforged.neoforge.client.blaze3d.validation.ValidationRenderPass;
 
 @Mixin(ValidationCommandEncoder.class)
 public abstract class ValidationCommandEncoderMixin implements CommandEncoderExtended {
     @Shadow @Final private CommandEncoder realCommandEncoder;
+    @Shadow @Final private GpuDeviceUsageValidator validator;
+
+    @Shadow protected ValidationRenderPass wrapRenderPass(RenderPass renderPass, GpuDeviceUsageValidator validator) { return null; }
 
     @Override
     public RenderPass canpipe_createRenderPass(
         Supplier<String> supplier, GpuTextureView[] colorAttachments, @Nullable GpuTextureView depthAttachment
     ) {
-        return ((CommandEncoderExtended) this.realCommandEncoder).canpipe_createRenderPass(
+        colorAttachments = colorAttachments.clone();
+
+        for (int i = 0; i < colorAttachments.length; ++i) {
+            if (colorAttachments[i] instanceof ValidationGpuTextureView validationTextureView) {
+                colorAttachments[i] = validationTextureView.getRealTextureView();
+            }
+        }
+
+        var renderPass = ((CommandEncoderExtended) this.realCommandEncoder).canpipe_createRenderPass(
             supplier, colorAttachments, depthAttachment
         );
+
+        return wrapRenderPass(renderPass, validator);
     }
 
     @Override
