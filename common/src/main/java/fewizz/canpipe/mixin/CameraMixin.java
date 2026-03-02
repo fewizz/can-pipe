21cca254aa86f78b2237eea9305f66e299863eb7
fewizz <hazeevaidar@gmail.com>
2026-03-02 22:04:46 +0300

    bring back first frame `Camera.fovModifier` value fix

--- common/src/main/java/fewizz/canpipe/mixin/CameraMixin.java
+++ common/src/main/java/fewizz/canpipe/mixin/CameraMixin.java
@@ -4,6 +4,9 @@ import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
@@ -14,8 +17,21 @@ import net.minecraft.client.Minecraft;
 @Mixin(Camera.class)
 public class CameraMixin implements CameraExtended {
 
-    @Shadow @Final private Minecraft minecraft = Minecraft.getInstance();
+    @Shadow @Final private Minecraft minecraft;
     @Shadow private float fov;
+    @Shadow private float fovModifier;
+
+    @Shadow private void tickFov() {}
+
+    @Inject(method = "update", at = @At("RETURN"))
+    void onUpdate(CallbackInfo ci) {
+        // on first frame `this.calculateFov()` will return 0.0F,
+        // because `this.tickFov()` wasn't called and this.fovModifier is 0.0F
+        // That will lead to invalid projection matrix
+        if (this.fovModifier == 0.0F) {
+            this.tickFov();
+        }
+    }
 
     @Override
     public Matrix4f canpipe_createProjectionMatrixForCulling(float depthFar) {
