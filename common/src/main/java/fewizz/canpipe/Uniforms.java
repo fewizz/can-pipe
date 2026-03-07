03a342baddc2450bba941f509dcf59a92a7ad2e0
fewizz <hazeevaidar@gmail.com>
2026-03-08 00:18:16 +0300

    fix frx_worldDay and frx_worldTime

--- common/src/main/java/fewizz/canpipe/Uniforms.java
+++ common/src/main/java/fewizz/canpipe/Uniforms.java
@@ -362,8 +362,8 @@ public class Uniforms {
         // world
         long ticks = mc.level.getDefaultClockTime();
         CANPIPE_FIXED_OR_DAY_TIME.set((ticks % 24000L) / 24000.0F);
-        FRX_WORLD_DAY.set(mc.level != null ? (mc.level.getGameTime() / 24000L) % 2147483647L : 0.0F);
-        FRX_WORLD_TIME.set(mc.level != null ? (mc.level.getGameTime() % 24000L) / 24000.0F : 0.0F);
+        FRX_WORLD_DAY.set((float) ((ticks / 24000L) % 2147483647L));
+        FRX_WORLD_TIME.set((ticks % 24000L) / 24000.0F);
         FRX_MOON_SIZE.set(DimensionType.MOON_BRIGHTNESS_PER_PHASE[mc.gameRenderer.getGameRenderState().levelRenderState.skyRenderState.moonPhase.index()]);
         FRX_SKY_LIGHT_VECTOR.set(p.getSunOrMoonDir(mc.level, new Vector3f()));
         FRX_SKY_ANGLE_RADIANS.set(mc.gameRenderer.getGameRenderState().levelRenderState.skyRenderState.sunAngle);
