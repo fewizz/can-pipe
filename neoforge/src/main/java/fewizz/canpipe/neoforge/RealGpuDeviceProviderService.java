ec7c5c344db44cc834f378b52e1316e83be2f270
fewizz <hazeevaidar@gmail.com>
2026-03-26 19:01:52 +0300

    update to 26.1

--- neoforge/src/main/java/fewizz/canpipe/neoforge/RealGpuDeviceProviderService.java
+++ neoforge/src/main/java/fewizz/canpipe/neoforge/RealGpuDeviceProviderService.java
@@ -1,19 +1,16 @@
 package fewizz.canpipe.neoforge;
 
 import com.mojang.blaze3d.systems.GpuDevice;
+import com.mojang.blaze3d.systems.GpuDeviceBackend;
 import com.mojang.blaze3d.systems.RenderSystem;
 
-import net.neoforged.neoforge.client.blaze3d.validation.ValidationGpuDevice;
+import fewizz.canpipe.b3d.mixin.GpuDeviceAccessor;
 
 public class RealGpuDeviceProviderService extends fewizz.canpipe.b3d.RealGpuDeviceProviderService {
 
     @Override
-    public GpuDevice realGpuDevice() {
-        var device = RenderSystem.getDevice();
-        if (device instanceof ValidationGpuDevice) {
-            device = ((ValidationGpuDevice) device).getRealDevice();
-        }
-        return device;
+    public GpuDeviceBackend impl_getRealGpuDeviceBackend() {
+        return ((GpuDeviceAccessor) RenderSystem.getDevice()).canpipe_getBackend();
     }
     
 }
