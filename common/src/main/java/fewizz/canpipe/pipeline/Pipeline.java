d85fdb3fc5195b6984fa743a4d818aae52622df0
fewizz <hazeevaidar@gmail.com>
2026-05-21 23:50:35 +0300

    always provide `Sampler1`

--- common/src/main/java/fewizz/canpipe/pipeline/Pipeline.java
+++ common/src/main/java/fewizz/canpipe/pipeline/Pipeline.java
@@ -567,6 +567,7 @@ public class Pipeline implements AutoCloseable {
 
         renderPass.setUniform("canpipe_ub_render_target", RenderSystemAccessor.canpipe_get0to3UBOBuffers()[renderTarget]);
 
+        renderPass.bindTexture("Sampler1", Minecraft.getInstance().gameRenderer.overlayTexture().getTextureView(), RenderSystem.getSamplerCache().getClampToEdge(FilterMode.LINEAR));
         renderPass.bindTexture("Sampler2", Minecraft.getInstance().gameRenderer.lightmap(), RenderSystem.getSamplerCache().getClampToEdge(FilterMode.LINEAR));
 
         for (var e : this.materialProgramSamplerTextures.entrySet()) {
