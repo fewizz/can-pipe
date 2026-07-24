3de2cc6dcc54d241a517705a635e77c91599990f
fewizz <hazeevaidar@gmail.com>
2026-07-25 01:14:07 +0300

    spv patching finale

--- common/src/main/java/fewizz/canpipe/b3d/mixin/VkGlslCompilerMixin.java
+++ common/src/main/java/fewizz/canpipe/b3d/mixin/VkGlslCompilerMixin.java
@@ -7,24 +7,19 @@ import java.nio.IntBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.lwjgl.system.MemoryUtil;
-import org.lwjgl.vulkan.VkDevice;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import com.google.common.base.Utf8;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.GpuDeviceBackend;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vulkan.VulkanDevice;
 import com.mojang.blaze3d.vulkan.glsl.GlslCompiler;
 
-import fewizz.canpipe.b3d.GpuDeviceExtended;
 import it.unimi.dsi.fastutil.bytes.ByteArrayList;
 import it.unimi.dsi.fastutil.bytes.ByteList;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
@@ -33,8 +28,6 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 
 @Mixin(GlslCompiler.class)
 public class VkGlslCompilerMixin {
@@ -69,24 +62,40 @@ public class VkGlslCompilerMixin {
 
         IntBuffer spvWords = spvBytes.asIntBuffer();
 
-        // 1. Search for `OpName`s (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpName)
+        int bound = spvWords.get(3);
 
-        Int2ObjectMap<String> idToName = new Int2ObjectOpenHashMap<>();
-
+        List<int[]> instrs = new ArrayList<>();
         for (int i = 5; i < spvWords.capacity();) {
             int wordCountAndOp = spvWords.get(i);
+            int wordCount = wordCountAndOp >>> 16;
+
+            int[] instr = new int[wordCount];
+            spvWords.get(i, instr);
+            instrs.add(instr);
+
+            i += wordCount;
+        }
+
+        // 1. Search for `OpName`s (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpName)
+        // and `OpTypePointer`s (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpTypePointer)
+
+        Int2ObjectMap<String> idToName = new Int2ObjectOpenHashMap<>();
+        Int2ObjectMap<int[]> typeIdToInstr = new Int2ObjectOpenHashMap<>();
+
+        for (int[] instr : instrs) {
+            int wordCountAndOp = instr[0];
 
             int wordCount = wordCountAndOp >>> 16;
             int op = wordCountAndOp & ((1 << 16) - 1);
 
             if (op == 5) {
-                int id = spvWords.get(i+1);
+                int id = instr[1];
 
                 ByteList chars = new ByteArrayList();
                 boolean done = false;
 
                 for (int x = 2; !done && x < wordCount; ++x) {
-                    int v = spvWords.get(i+x);
+                    int v = instr[x];
                     for (int s = 0; s < 4; ++s) {
                         byte b = (byte)((v >> (s*8)) & 0xFF);
                         if (b == 0) {
@@ -107,145 +116,130 @@ public class VkGlslCompilerMixin {
                 idToName.put(id, name);
             }
 
-            i += wordCount;
+            if (op == 32) {
+                int id = instr[1];
+                typeIdToInstr.put(id, instr);
+            }
         }
 
         // 2. Change storage class (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#Storage_Class)
         // of unused input `OpVariable`s (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpVariable)
         // from `Input` to `Private`
+        // Also create new type ptrs
 
-        IntSet typePointersIDsToPatch = new IntOpenHashSet();
         IntSet variablesIDsToPatch = new IntOpenHashSet();
 
-        for (int i = 5; i < spvWords.capacity();) {
-            int wordCountAndOp = spvWords.get(i);
+        for (int i = 0; i < instrs.size(); ++i) {
+            int[] instr = instrs.get(i);
 
-            int wordCount = wordCountAndOp >>> 16;
-            int op = wordCountAndOp & ((1 << 16) - 1);
+            int op = instr[0] & ((1 << 16) - 1);
 
             if (op == 59) {
-                int id = spvWords.get(i+2);
-                boolean isInput = spvWords.get(i+3) == 1;
+                int id = instr[2];
+                int typeId = instr[1];
+                boolean isInput = instr[3] == 1;
                 String name = idToName.get(id);
 
                 if (isInput && !attribs.contains(name)) {
-                    spvWords.put(i+3, 6);
-                    typePointersIDsToPatch.add(spvWords.get(i+1));
-                    variablesIDsToPatch.add(spvWords.get(i+2));
+                    instr[3] = 6;  // Private
+                    variablesIDsToPatch.add(id);
+
+                    int[] originalTypeInstr = typeIdToInstr.get(typeId);
+                    int[] newTypeInstr = new int[originalTypeInstr.length];
+                    System.arraycopy(originalTypeInstr, 0, newTypeInstr, 0, originalTypeInstr.length);
+
+                    newTypeInstr[2] = 6;  // Private
+                    newTypeInstr[1] = bound;
+                    instr[1] = bound;  // Point to new type op
+                    ++bound;
+
+                    instrs.add(i, newTypeInstr);
+                    ++i;
                 }
             }
-
-            i += wordCount;
         }
 
-        // 3. Patch `OpTypePointer`s (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpTypePointer)
+        // 3. Patch `OpEntryPoint`'s interface (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpEntryPoint)
 
-        for (int i = 5; i < spvWords.capacity();) {
-            int wordCountAndOp = spvWords.get(i);
+        for (int i = 0; i < instrs.size(); ++i) {
+            int[] instr = instrs.get(i);
 
-            int wordCount = wordCountAndOp >>> 16;
-            int op = wordCountAndOp & ((1 << 16) - 1);
+            int op = instr[0] & ((1 << 16) - 1);
 
-            if (op == 32) {
-                int id = spvWords.get(i+1);
-                if (typePointersIDsToPatch.contains(id)) {
-                    int storageClass = spvWords.get(i+2);
-                    if (storageClass != 1) { throw new RuntimeException("Expected storage class 1 (Input), but got "+storageClass); }
-                    spvWords.put(i+2, 6);
+            if (op != 15) continue;
+
+            int x = 3;
+            for (; x < instr.length; ++x) {
+                int v = instr[x];
+                byte[] arr = new byte[]{
+                    (byte)((v >>  0) & 0xFF),
+                    (byte)((v >>  8) & 0xFF),
+                    (byte)((v >> 16) & 0xFF),
+                    (byte)((v >> 24) & 0xFF)
+                };
+                if (arr[0] == 0 || arr[1] == 0 || arr[2] == 0 || arr[3] == 0) { break; }
+            }
+
+            int beginning = x;
+            IntList remainingIndices = new IntArrayList();
+
+            for (; x < instr.length; ++x) {
+                int id = instr[x];
+                if (!variablesIDsToPatch.contains(id)) {
+                    remainingIndices.add(id);
                 }
             }
 
-            i += wordCount;
+            // Patch instruction word count
+            int newWordCount = instr.length - (x - beginning) + remainingIndices.size();
+
+            int[] newInstr = new int[newWordCount];
+            System.arraycopy(instr, 0, newInstr, 0, beginning);
+
+            newInstr[0] = (newWordCount << 16) | op;
+
+            for (int j = 0; beginning+j < x; ++j) {
+                if (j < remainingIndices.size()) {
+                    newInstr[beginning+j] = remainingIndices.getInt(j);
+                }
+            }
+
+            instrs.set(i, newInstr);
+            break;
         }
 
-        // 4. Patch `OpEntryPoint`'s interface (https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpEntryPoint)
+        // 4. Patch decorations
 
-        IntSet wordsToSkip = new IntOpenHashSet();
+        for (int i = 0; i < instrs.size();) {
+            int[] instr = instrs.get(i);
 
-        for (int i = 5; i < spvWords.capacity();) {
-            int wordCountAndOp = spvWords.get(i);
-
-            int wordCount = wordCountAndOp >>> 16;
-            int op = wordCountAndOp & ((1 << 16) - 1);
-
-            if (op == 15) {
-                int x = 3;
-                for (; x < wordCount; ++x) {
-                    int v = spvWords.get(i+x);
-                    byte[] arr = new byte[]{
-                        (byte)((v >>  0) & 0xFF),
-                        (byte)((v >>  8) & 0xFF),
-                        (byte)((v >> 16) & 0xFF),
-                        (byte)((v >> 24) & 0xFF)
-                    };
-                    if (arr[0] == 0 || arr[1] == 0 || arr[2] == 0 || arr[3] == 0) { break; }
-                }
-
-                int beginning = x;
-                IntList remainingIndices = new IntArrayList();
-
-                for (; x < wordCount; ++x) {
-                    int id = spvWords.get(i+x);
-                    if (!variablesIDsToPatch.contains(id)) {
-                        remainingIndices.add(id);
-                    }
-                }
-
-                // Patch instruction word count
-                int newWordCount = wordCount - (x - beginning) + remainingIndices.size();
-                spvWords.put(i, (newWordCount << 16) | op);
-
-                for (int j = 0; beginning+j < x; ++j) {
-                    if (j < remainingIndices.size()) {
-                        spvWords.put(i+beginning+j, remainingIndices.getInt(j));
-                    }
-                    else {
-                        wordsToSkip.add(i+beginning+j);
-                    }
-                }
-
-                break;
-            }
-
-            i += wordCount;
-        }
-
-        // 5. Patch decorations
-
-        for (int i = 5; i < spvWords.capacity();) {
-            if (wordsToSkip.contains(i)) {
-                ++i;
-                continue;
-            }
-
-            int wordCountAndOp = spvWords.get(i);
-
-            int wordCount = wordCountAndOp >>> 16;
-            int op = wordCountAndOp & ((1 << 16) - 1);
+            int op = instr[0] & ((1 << 16) - 1);
 
             if (op == 71) {
-                int id = spvWords.get(i+1);
-                boolean isLocation = spvWords.get(i+2) == 30;
+                int id = instr[1];
+                boolean isLocation = instr[2] == 30;
                 if (isLocation && variablesIDsToPatch.contains(id)) {
-                    for (int x = 0; x < wordCount; ++x) wordsToSkip.add(i+x);
+                    instrs.remove(i);
+                    continue;
                 }
             }
 
-            i += wordCount;
+            ++i;
         }
 
-        int newSize = spvWords.limit() - wordsToSkip.size();
+        int newSize = 5 + instrs.stream().mapToInt(i -> i.length).sum();
 
         if (newSize != spvWords.limit()) {
             ByteBuffer newSpvBytes = MemoryUtil.memAlloc(newSize*4);
             IntBuffer newSpvWords = newSpvBytes.asIntBuffer();
 
-            for (int i = 0, j = 0; i < spvWords.limit(); ++i) {
-                int word = spvWords.get(i);
-                if (!wordsToSkip.contains(i)) {
-                    newSpvWords.put(j, word);
-                    ++j;
-                }
+            newSpvWords.put(0, spvWords, 0, 5);
+            newSpvWords.put(3, bound);
+
+            int i = 5;
+            for (int[] instr : instrs) {
+                newSpvWords.put(i, instr);
+                i += instr.length;
             }
 
             MemoryUtil.memFree(spvBytes);
